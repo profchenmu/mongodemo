6a5e196fcc9f431df6200a9e0d32fcf84bf2d70a
lptm <lptm@lptms-MBP.Home>
2016-08-02 23:40:33 +0800

    test

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var cao = require('./routes/cao');
